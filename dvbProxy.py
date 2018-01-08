4bd634cb4d839899f3092bdb4d9c1ba5abcf8ba1
Philip Lawall <philiplawall@gmail.com>
2018-01-08 19:32:06 +0100

    DVBLink update

--- dvbProxy.py
+++ dvbProxy.py
@@ -1,17 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from gevent import monkey; monkey.patch_all()
 
 import time
 import os
 import requests
+import sys
 from gevent.pywsgi import WSGIServer
 from flask import Flask, Response, request, jsonify, abort, render_template
+
+import untangle
+import code
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
 # URL format: <protocol>://<username>:<password>@<hostname>:<port>, example: https://test:1234@localhost:9981
 config = {
     'bindAddr': os.environ.get('DVB_BINDADDR') or '',
-    'dvbURL': os.environ.get('DVB_URL') or 'http://localhost:8100',
+    'dvbURL': os.environ.get('DVB_URL') or 'http://192.168.1.11:8100',
+    'dvbStreamURL': os.environ.get('DVB_STREAM_URL') or 'http://192.168.1.11:8101',
     'dvbProxyURL': os.environ.get('DVB_PROXY_URL') or 'http://localhost',
     'tunerCount': os.environ.get('DVB_TUNER_COUNT') or 6,  # number of tuners in DVBLink
     'dvbWeight': os.environ.get('DVB_WEIGHT') or 300,  # subscription priority
@@ -43,7 +54,7 @@ def status():
         'ScanInProgress': 0,
         'ScanPossible': 1,
         'Source': "Cable",
-        'SourceList': ['Cable']
+        'SourceList': ['Cable', 'Satellite']
     })
 
 
@@ -53,7 +64,7 @@ def lineup():
 
     for c in _get_channels():
         if c['enabled']:
-            url = '%s/stream/channel/%s?profile=%s&weight=%s' % (config['dvbURL'], c['uuid'], config['streamProfile'],int(config['dvbWeight']))
+            url = '%s/dvblink/direct?channel=%s' % (config['dvbURL'], c['id'])
 
             lineup.append({'GuideNumber': str(c['number']),
                            'GuideName': c['name'],
@@ -63,7 +74,7 @@ def lineup():
     return jsonify(lineup)
 
 
-@app.route('/lineup.post', methods=[‘GET’, ‘POST’])
+@app.route('/lineup.post', methods=['GET', 'POST'])
 def lineup_post():
     return ''
 
@@ -75,10 +86,30 @@ def device():
 
 def _get_channels():
     url = '%s/api/channel/grid?start=0&limit=999999' % config['dvbURL']
+    url = '%s/cs/' % config['dvbURL']
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    data = {'command': 'get_channels'}
 
     try:
-        r = requests.get(url)
-        return r.json()['entries']
+        r = requests.post(url, headers=headers, data=data)
+        tree = untangle.parse(r.content)
+        d = tree.response.xml_result.cdata
+        dd = untangle.parse(d)
+
+        entries = []
+
+        for channel in dd.channels.channel:
+            if int(channel.channel_number.cdata) > 0:
+                entries.append({'enabled': True,
+                                'id': channel.channel_id.cdata,
+                                'dvblink_id': channel.channel_dvblink_id.cdata,
+                                'name': channel.channel_name.cdata,
+                                'logo': channel.channel_logo.cdata,
+                                'type': channel.channel_type.cdata,
+                                'number': channel.channel_number.cdata,
+                                'subnumber': channel.channel_subnumber.cdata})
+
+        return entries
 
     except Exception as e:
         print('An error occured: ' + repr(e))
