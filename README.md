a26d736ffe8f8d43f303a8ab5bf94c3c0cee7b3b
Philip Lawall <philiplawall@gmail.com>
2018-01-08 16:07:09 +0100

    rename to dvbProxy

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 tvhProxy
 ========
 
-A small flask app to proxy requests between Plex Media Server and Tvheadend.
+A small flask app to proxy requests between Plex Media Server and DVBLink Server.
 
 #### tvhProxy configuration
-1. In tvhProxy.py configure options as per your setup.
+1. In dvbProxy.py configure options as per your setup.
 2. Create a virtual enviroment: ```$ virtualenv venv```
 3. Activate the virtual enviroment: ```$ . venv/bin/activate```
 4. Install the requirements: ```$ pip install -r requirements.txt```
-5. Finally run the app with: ```$ python tvhProxy.py```
+5. Finally run the app with: ```$ python dvbProxy.py```
 
 #### systemd service configuration
-A startup script for Ubuntu can be found in tvhProxy.service (change paths in tvhProxy.service to your setup), install with:
+A startup script for Ubuntu can be found in dvbProxy.service (change paths in dvbProxy.service to your setup), install with:
 
-    $ sudo cp tvhProxy.service /etc/systemd/system/tvhProxy.service
+    $ sudo cp dvbProxy.service /etc/systemd/system/dvbProxy.service
     $ sudo systemctl daemon-reload
-    $ sudo systemctl enable tvhProxy.service
-    $ sudo systemctl start tvhProxy.service
+    $ sudo systemctl enable dvbProxy.service
+    $ sudo systemctl start dvbProxy.service
 
 #### Plex configuration
-Enter the IP of the host running tvhProxy including port 5004, eg.: ```192.168.1.50:5004```
+Enter the IP of the host running dvbProxy including port 5004, eg.: ```192.168.1.50:5004```
--- dvbProxy.py
+++ dvbProxy.py
@@ -10,17 +10,17 @@ app = Flask(__name__)
 
 # URL format: <protocol>://<username>:<password>@<hostname>:<port>, example: https://test:1234@localhost:9981
 config = {
-    'bindAddr': os.environ.get('TVH_BINDADDR') or '',
-    'tvhURL': os.environ.get('TVH_URL') or 'http://test:test@localhost:9981',
-    'tvhProxyURL': os.environ.get('TVH_PROXY_URL') or 'http://localhost',
-    'tunerCount': os.environ.get('TVH_TUNER_COUNT') or 6,  # number of tuners in tvh
-    'tvhWeight': os.environ.get('TVH_WEIGHT') or 300,  # subscription priority
-    'chunkSize': os.environ.get('TVH_CHUNK_SIZE') or 1024*1024,  # usually you don't need to edit this
-    'streamProfile': os.environ.get('TVH_PROFILE') or 'pass'  # specifiy a stream profile that you want to use for adhoc transcoding in tvh, e.g. mp4
+    'bindAddr': os.environ.get('DVB_BINDADDR') or '',
+    'dvbURL': os.environ.get('DVB_URL') or 'http://localhost:8100',
+    'dvbProxyURL': os.environ.get('DVB_PROXY_URL') or 'http://localhost',
+    'tunerCount': os.environ.get('DVB_TUNER_COUNT') or 6,  # number of tuners in DVBLink
+    'dvbWeight': os.environ.get('DVB_WEIGHT') or 300,  # subscription priority
+    'chunkSize': os.environ.get('DVB_CHUNK_SIZE') or 1024*1024,  # usually you don't need to edit this
+    'streamProfile': os.environ.get('DVB_PROFILE') or 'pass'  # specifiy a stream profile that you want to use for adhoc transcoding in DVBLink, e.g. mp4
 }
 
 discoverData = {
-    'FriendlyName': 'tvhProxy',
+    'FriendlyName': 'dvbProxy',
     'Manufacturer' : 'Silicondust',
     'ModelNumber': 'HDTC-2US',
     'FirmwareName': 'hdhomeruntc_atsc',
@@ -28,8 +28,8 @@ discoverData = {
     'FirmwareVersion': '20150826',
     'DeviceID': '12345678',
     'DeviceAuth': 'test1234',
-    'BaseURL': '%s' % config['tvhProxyURL'],
-    'LineupURL': '%s/lineup.json' % config['tvhProxyURL']
+    'BaseURL': '%s' % config['dvbProxyURL'],
+    'LineupURL': '%s/lineup.json' % config['dvbProxyURL']
 }
 
 @app.route('/discover.json')
@@ -53,7 +53,7 @@ def lineup():
 
     for c in _get_channels():
         if c['enabled']:
-            url = '%s/stream/channel/%s?profile=%s&weight=%s' % (config['tvhURL'], c['uuid'], config['streamProfile'],int(config['tvhWeight']))
+            url = '%s/stream/channel/%s?profile=%s&weight=%s' % (config['dvbURL'], c['uuid'], config['streamProfile'],int(config['dvbWeight']))
 
             lineup.append({'GuideNumber': str(c['number']),
                            'GuideName': c['name'],
@@ -74,7 +74,7 @@ def device():
 
 
 def _get_channels():
-    url = '%s/api/channel/grid?start=0&limit=999999' % config['tvhURL']
+    url = '%s/api/channel/grid?start=0&limit=999999' % config['dvbURL']
 
     try:
         r = requests.get(url)
